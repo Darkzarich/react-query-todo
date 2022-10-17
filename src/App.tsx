68f3f31d0a0e320b2defbc3d7c766524ad3f969b
Darkzarich <darkzarichv2@gmail.com>
2022-10-17 10:06:39 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import { useState } from 'react'
+import Header from './Header'
 import './App.css'
 
 function App() {
-  return <div className="text-3xl text-red-400 text-center">test</div>
+  return (
+    <>
+      <Header />
+      <div className="container mx-auto mt-4">
+        <div className="bg-slate-600 rounded-md p-5">
+          <label className="">
+            <input type="text" className="bg-slate-700 rounded-md" />
+          </label>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App
---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="px-8 py-4 h-[61px] bg-slate-600 text-white text-2xl">
+      React Todo App
+    </header>
+  )
+}
+
+export default Header
